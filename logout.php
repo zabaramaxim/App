bca4370ff09f75bca60b973eff7de97acb4472fe
zabaramaxim <90805053+zabaramaxim@users.noreply.github.com>
2022-03-28 18:58:26 +0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -4,5 +4,5 @@ session_start();
 unset($_SESSION['user']);
 session_destroy();
 setcookie('user', ' ', time() - 3600, '/');
-header('Location: /ManaoTest/login_view.php');
+header('Location: /App/login_view.php');
 ?>
