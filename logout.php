6e02eb8cf290e08228c217c24e3ab72e3f2760c2
vor20 <zabaramaksim35@gmail.com>
2022-03-28 18:53:38 +0300

    first commit

---
+++ logout.php
@@ -0,0 +1,8 @@
+<?php
+//use App\DatabaseManager;
+session_start();
+unset($_SESSION['user']);
+session_destroy();
+setcookie('user', ' ', time() - 3600, '/');
+header('Location: /ManaoTest/login_view.php');
+?>
